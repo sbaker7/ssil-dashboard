7dbfa2d0eedbbac655a118cf894a3414a334fa00
Alex Cummaudo <alexcu@me.com>
2015-06-24 18:04:45 +1000

    ENHANCE: Added extra checks to resolve the Lilydale/Belgrave/Ringwood issue; now ensuring that up/down trains being considered in same-time trains

--- jobs/ptv.rb
+++ jobs/ptv.rb
@@ -15,6 +15,7 @@ SCHEDULER.every '1m', first_in: '1s' do |job|
   api.health_check
   departures = api.broad_next_departures(PtvTimetable::TRAIN, GLENFERRIE_STOP_ID).values.first
   departures = departures.map do | departure |
+    linedir_id = departure['platform']['direction']['linedir_id']
     skipped_stations = departure['run']['num_skipped']
     destination_name = departure['run']['destination_name']
     destination_id = departure['run']['destination_id']
@@ -37,8 +38,9 @@ SCHEDULER.every '1m', first_in: '1s' do |job|
         'stopping all stations'
       end
     # work out heading to
-    heading_to = departure['platform']['direction']['direction_name']
-    to_city    = departure['platform']['direction']['direction_id'] == 0
+    heading_to   = departure['platform']['direction']['direction_name']
+    direction_id = departure['platform']['direction']['direction_id']
+    to_city      = direction_id == 0
     # remove "City" to get terminus station name in city
     heading_to = heading_to.gsub(/City|[()]|^\s|$\s/, '').strip! if to_city
     # get the timetable departing time
@@ -50,10 +52,14 @@ SCHEDULER.every '1m', first_in: '1s' do |job|
     end
     {
       destination_name:     destination_name,
+      line_id:              line_id,
+      linedir_id:           linedir_id,
+      skipped_stations:     skipped_stations,
       destination_distance: destination_distance,
       express:              express,
       heading_to:           heading_to,
       to_city:              to_city,
+      direction_id:         direction_id,
       time: {
         timetable:          time_timetable,
         realtime:           time_realtime || nil
@@ -62,18 +68,30 @@ SCHEDULER.every '1m', first_in: '1s' do |job|
   end
   # if two trains leave at the same time, choose the departure that goes the furtherest
   reduced_departures = []
-  departures.group_by { | departure | departure[:time][:timetable] }.values.each do | group |
+  # so, we will group by
+  # - timetable departure time (they leave at the same time)
+  # - to_city: they need to be either going up (to city) or down (out from city)
+  departures.group_by { | departure | [departure[:time][:timetable], departure[:express], departure[:to_city], departure[:time][:line_id]] }.values.each do | group |
     # if there is only one train leaving at this time then that's okay
+    puts ">>>>"
     if group.length == 1
+      puts group
+      # this better not be Camberwell! Camberwell is not a terminus!
+      if group.first[:destination_name] == "Camberwell"
+        puts "<<<< not resolving as Camberwell is not a terminus!"
+        next
+      end
       reduced_departures.push group.first
+      puts "<<<< auto resolved #{group.first[:time][:timetable]} to #{group.first[:destination_name]}"
       next
     end
-    # TODO: need to check if to city or not to city!!!!
-    puts ">>>>> "
-    puts group.sort_by { |departure| departure[:destination_distance] }
-    last = group.sort_by { |departure| departure[:destination_distance] }.last
-    reduced_departures.push last
-    puts "<<<< resolved to #{last[:destination_name]}"
+    # otherwise to resolve the lilydale/belgrave/ringwood issue, we will choose
+    # the departure who travel's the furtherest (longest destination distance)
+    sorted_by_distance = group.sort_by { |departure| departure[:destination_distance] }
+    puts sorted_by_distance
+    furtherest_destination_departure = sorted_by_distance.last
+    reduced_departures.push furtherest_destination_departure
+    puts "<<<< resolved #{group.first[:time][:timetable]} to #{furtherest_destination_departure[:destination_name]}"
   end
   reduced_departures = reduced_departures.sort_by { | departure | departure[:time][:time_timetable] }
   send_event('ptv', departures: reduced_departures)
