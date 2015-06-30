25d77b53f7a022a8b82c2ce6613ac6dde655d750
Alex Cummaudo <alexcu@me.com>
2015-06-30 16:16:37 +1000

    LOOKS: Update the looks of the whereabouts widget

--- jobs/whereabouts.rb
+++ jobs/whereabouts.rb
@@ -1,12 +1,15 @@
 require 'net/http'
 require 'json'
 
-WHEREABOUTS_URI = ENV["WHEREABOUTS_URI"]
-SCHEDULER.every '2s', first_in: '1s' do |job|
+WHEREABOUTS_URI = "http://128.199.89.147:3000/states" #ENV["WHEREABOUTS_URI"]
+SCHEDULER.every '10s', first_in: '1s' do |job|
   uri = URI(WHEREABOUTS_URI)
   whereabouts_data = JSON.parse Net::HTTP.get(uri)
-  sick_people = whereabouts_data['staying_home'].select { |data| data['info'].nil? }.map { | data | data['user'] }
-  home_people = whereabouts_data['staying_home'].select { |data| data['info'] == 'working_at_home' }.map { | data | data['user'] }
-  late_people = whereabouts_data['running_late'].map { | data | data['user'] }
-  send_event('whereabouts', sick: sick_people, late: late_people, home: home_people)
+
+  sick = whereabouts_data['staying_home']
+  home = whereabouts_data['working_at_home']
+  late = whereabouts_data['running_late']
+  offsite = whereabouts_data['offsite']
+
+  send_event('whereabouts', sick: sick, home: home, late: late, offsite: offsite)
 end
