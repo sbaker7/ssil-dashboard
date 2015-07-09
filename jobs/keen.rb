f7a2ec1ec10d4a2dee4dbdcc9877acf9cb63d2e0
Alex Cummaudo <alexcu@me.com>
2015-07-09 18:08:05 +1000

    ENHANCE: Updated keen bot to respond back commands

---
+++ jobs/keen.rb
@@ -0,0 +1,90 @@
+class Keeners
+  def initialize
+    @list = {}
+    @timeout = 30 # mins
+    @clear = nil
+  end
+  def add(user_id, who_keened)
+    @list[user_id] = { time: Time.now.to_i, name: who_keened }
+    Thread.kill @clear unless @clear.nil?
+    @clear = Thread.new do
+      sleep @timeout * 60
+      # Clear the keeners after 30 mins
+      @list = {}
+      keeners.ping_keen
+    end
+    @list[user_id]
+  end
+  def to_s
+    '<@' << @list.keys.join('>, <@') << '>'
+  end
+  def remove(user_id)
+    @list.delete user_id
+  end
+  def list
+    @list
+  end
+  def last
+    data = @list.sort_by { |uid, data| data[:time] }.last
+    unless data.nil?
+      return data.last
+    else
+      return nil
+    end
+  end
+  def ping_keen(play_sound = false)
+    Thread.new do
+      send_event('keen', { keener: last, play_sound: play_sound, keen_count: @list.length } )
+    end
+  end
+end
+keeners = Keeners.new
+before '/widgets/keen' do
+  # Set the auth token
+  request.params["auth_token"] = request.params["token"]
+  # Make sure request response.body =is in JSON
+  request.body.string = request.params.to_json
+end
+get '/widgets/keen' do
+  status 200
+  # Register who's keen
+  who_keened    = request.params["user_name"]
+  user_id       = request.params["user_id"]
+  keen_param    = request.params["text"]
+  case keen_param
+  # No keen parameter -- keening for the first time
+  when ''
+    if keeners.list.keys.include? user_id
+      response.body = "But you're already :caffkeen:?"
+    else
+      data = keeners.add user_id, who_keened
+      if keeners.list.length == 1
+        response.body = "You're the first to :caffkeen: up!"
+      else
+        response.body = ":caffkeen:: " << keeners.to_s
+      end
+      keeners.ping_keen true
+    end
+  when 'clear'
+    unless keeners.list.keys.include? user_id
+      response.body = "But you're not :caffkeen:?"
+    else
+      keeners.remove user_id
+      response.body = "You un-:caffkeen:'ed!"
+      keeners.ping_keen
+    end
+  when 'who'
+    if keeners.list.length > 0
+      response.body = "Here's who's :caffkeen:'ed up:\n" << keeners.to_s
+    else
+      response.body = "No one is :caffkeen: right now :pensive:"
+    end
+  when 'help'
+    response.body = "How to :caffkeen::\n" <<
+                    "`/keen`\n_Make it known that your are :caffkeen:_\n\n" <<
+                    "`/keen who`\n_Lists who is keen_\n\n" <<
+                    "`/keen clear`\n_Undoes your last keen_\n\n"
+  else
+    response.body = "Wha?"
+  end
+end
--- widgets/keen/keen.coffee
+++ widgets/keen/keen.coffee
@@ -2,41 +2,15 @@ class Dashing.Keen extends Dashing.Widget
   # How many keen sound effects there are in /assets/keen
   KEEN_SOUNDS = 10
 
-  # How often to clear the keens, in mins
-  CLEAR_KEEN = 30
-  keens = {}
-
-  timeout = null
-
-  setupKeens: ->
-    # Reset the clearKeens interval
-    clearInterval timeout
-    timeout = setInterval @clearKeens, CLEAR_KEEN * 60000
-
-  clearKeens: =>
-    keens = {}
-    @set 'empty', true
-    @set 'keenCount', 0
-    @set 'whosKeen',  ''
-    @set 'lastKeen', ''
-
-  ready: ->
-    # Clear the keens object every CLEAR_KEEN mins
-    @clearKeens()
-    @setupKeens()
-
   onData: (data) ->
-    # Register who's keen
-    whosKeen = data.user_name
-    # Can't rekeen
-    # return if keens[whosKeen]?
-    keens[whosKeen] = moment()
-    @set 'empty', false
-    @set 'keenCount', Object.keys(keens).length
-    @set 'whosKeen' , "@" + whosKeen
-    @set 'lastKeen', moment().format('h:mm a')
-    # Reset the clearKeens interval
-    @setupKeens()
+    noKeeners = data.keen_count is 0
+    @set 'empty', noKeeners
+    return if noKeeners
+    console.log data.keener.time
+    @set 'keenCount', data.keen_count
+    @set 'whosKeen' , data.keener.name
+    @set 'lastKeen', moment.unix(data.keener.time).format('h:mm a')
+    return unless data.play_sound
     # Play the keen sound
     keenSound = Math.floor(Math.random() * KEEN_SOUNDS)
     new Audio("/sounds/keen/#{keenSound}.WAV").play()
